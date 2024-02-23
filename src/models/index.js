cd96e3f97a9ddbd63013ce91fe7b9d23dc3788c3
FrednerysMora <frednerysmr28@gmail.com>
2024-02-23 00:10:34 -0500

    advnaces

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
+const Cart = require("./Cart");
 const Category = require("./Category");
 const Product = require("./Product");
 const User = require("./User");
@@ -5,3 +6,11 @@ const User = require("./User");
 // Product -> categoryId
 Product.belongsTo(Category) // categoryId
 Category.hasMany(Product)
+
+//Cart -> userId
+Cart.belongsTo(User) //userId
+User.hasMany(Cart)
+
+//Cart -> ProductId
+Cart.belongsTo(Product) //productId
+Product.hasMany(Cart)
